b7cb067ff4b86321cc20262a213d9e451e2460f9
LilChara999 <annamarienkova33@gmail.com>
2020-10-09 08:22:38 +0200

    Изменения в странице с ответами на вопросы.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var giftRouter = require('./routes/gift');
+var faqRouter = require('./routes/faq');
 
 var mongoDB = 'mongodb://127.0.0.1/my_database';
 mongoose.connect(mongoDB);
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/gift', giftRouter);
+app.use('/faq', faqRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
