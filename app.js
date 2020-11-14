407e8802da2a71391d3bce730c9d75163d0b31c0
LilChara999 <annamarienkova33@gmail.com>
2020-11-14 19:34:29 +0100

    Добавлен код для регистрации и аутентификации

--- app.js
+++ app.js
@@ -3,19 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var dbConfig = require('./db.js');
 var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var faqRouter = require('./routes/faq');
 
-var mongoDB = 'mongodb://localhost/tutorial';
-mongoose.connect("mongodb://localhost:27017/tutorial", { useNewUrlParser: true, useUnifiedTopology: true });
+var mongoDB = 'mongodb://localhost:27017/userssessions';
+mongoose.connect(dbConfig.url), { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = require('bluebird');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
