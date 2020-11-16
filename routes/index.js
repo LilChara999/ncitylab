10583d117dfda31171da932459a828f5b4d14b89
LilChara999 <annamarienkova33@gmail.com>
2020-11-16 21:32:26 +0100

    Возврат.

--- routes/index.js
+++ routes/index.js
@@ -11,37 +11,37 @@ var isAuthenticated = function (req, res, next) {
 module.exports = function(passport){
 
 	/* GET login page. */
-	app.get('/', function(req, res) {
+	router.get('/', function(req, res) {
     	// Display the Login page with any flash message, if any
 		res.render('index', { message: req.flash('message') });
 	}),
 
 	/* Handle Login POST */
-	app.post('/login', passport.authenticate('login', {
+	router.post('/login', passport.authenticate('login', {
 		successRedirect: '/home',
 		failureRedirect: '/',
 		failureFlash : true
 	})),
 
 	/* GET Registration Page */
-	app.get('/signup', function(req, res){
+	router.get('/signup', function(req, res){
 		res.render('register',{message: req.flash('message')});
 	}),
 
 	/* Handle Registration POST */
-	app.post('/signup', passport.authenticate('signup', {
+	router.post('/signup', passport.authenticate('signup', {
 		successRedirect: '/home',
 		failureRedirect: '/signup',
 		failureFlash : true
 	})),
 
 	/* GET Home Page */
-	app.get('/home', isAuthenticated, function(req, res){
+	router.get('/home', isAuthenticated, function(req, res){
 		res.render('home', { user: req.user });
 	}),
 
 	/* Handle Logout */
-	app.get('/signout', function(req, res) {
+	router.get('/signout', function(req, res) {
 		req.logout();
 		res.redirect('/');
 	})
